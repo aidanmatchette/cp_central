cf6a1cda73f2346158a942b3d684886fad4a4640
Aidan Matchette <frigasack@gmail.com>
2022-04-29 16:16:48 -0700

    started sign-up page, installed dependecies for react, run npm install again to remain up to date.

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,7 +1,22 @@
 import {useState} from "react";
 import {signup} from "../utils/useAxios";
 import {useNavigate} from "react-router-dom";
-import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import { Button, CssBaseline, TextField, FormControl, Link, Grid, Box, Typography, InputLabel, Container, Select } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+export const theme = createTheme  ({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#118888',
+    },
+    secondary: {
+      main: '#ff5500',
+    },
+  },
+});
+
+
 
 function Signup() {
     const navigate = useNavigate()
@@ -24,29 +39,112 @@ function Signup() {
             setErrorMessage(errorText)
         }
     }
+    // const theme = createTheme();
 
     return (
-        <Container>
-        <Row>
-            <Col/>
-            <Col xs={6}>
-                <Row>
-                {errorMessage && <p className={"errorDetails"}>{errorMessage}</p>}
-                    <Form onSubmit={doSignUp}>
-                    <Form.Control size="lg" type="text" placeholder="First Name" name="firstName" />                          
-                        <Form.Control size="lg" type="text" placeholder="Last Name" name="lastName"/> 
-                        <Form.Control size="lg" type="text" placeholder="Email" name="email"/> 
-                        <Form.Control size="lg" type="text" placeholder="username" name="username" required/>                          
-                        <Form.Control size="lg" type="password" placeholder="password" name="password" required/>   
-                        <Form.Control size="lg" type="password" placeholder="password2" name="password2" required/>                           
-                        <Button variant="primary" type="submit">Submit</Button>                            
-                    </Form>
-                </Row>
-            </Col>
-            <Col/>
-        </Row>
-    </Container>
+           <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+            <img src="https://www.codeplatoon.org/wp-content/uploads/2018/10/CP-logo-2018-abbrev-1.png" width="200" alt='cp-logo'/>
+          <Typography component="h1" variant="h4">
+            Sign Up
+          </Typography>
+          <Box component="form" noValidate onSubmit={doSignUp} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password2"
+                  label="Confirm Password"
+                  type="password"
+                  id="password2"
+                  autoComplete="new-password"
+                />
+              </Grid>
+                {/* cohort selection */}
+              <Grid item xs={12}>
+                <FormControl fullWidth >
+                <InputLabel required id="cohort-select-label" >Select Cohort</InputLabel>
+                    <Select fullWidth label="Select Cohort" id="cohort-select">
+                        <option value={'test'} >Test Cohort(CHANGE ME)</option> 
+                    </Select>
+                </FormControl>  
+                
+              </Grid> 
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="center">
+              <Grid item>
+                <Link href="#/login" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        {/* <Copyright sx={{ mt: 5 }} /> */}
+      </Container>
+    </ThemeProvider>
     )
 }
 
 export default Signup
+
