afd9c1189e57b525e2692075a46dc4a731ae99b2
John Winko <john.winko@gmail.com>
2022-04-30 21:24:38 -0500

    added models to admin

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from .models import User, Appointment, UserLink
+from .models import User, Appointment, UserLink, TopicLink, Topic, Vote, Question, QuestionResponse, QuestionOption, \
+    Lesson, Questionnaire, FilledQuestionnaire, Feedback, ActivityGroup, Activity
 
 admin.site.register(User)
 admin.site.register(Appointment)
 admin.site.register(UserLink)
+admin.site.register(TopicLink)
+admin.site.register(Topic)
+admin.site.register(Vote)
+admin.site.register(Question)
+admin.site.register(QuestionResponse)
+admin.site.register(QuestionOption)
+admin.site.register(Lesson)
+admin.site.register(Questionnaire)
+admin.site.register(FilledQuestionnaire)
+admin.site.register(Feedback)
+admin.site.register(ActivityGroup)
+admin.site.register(Activity)
