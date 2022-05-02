0dde27e1df17b17c0e8b2e459e23e91faef72e31
Aidan Matchette <frigasack@gmail.com>
2022-05-02 10:14:28 -0700

    added rough idea of what data will be displayed to students

---
+++ src/pages/StudentPage.js
@@ -0,0 +1,22 @@
+import GoogleCalendar from "../components/GoogleCalendar";
+
+function StudentPage() {    
+    return (
+        <div>
+            <h1>This is the Authenticated student landing page</h1>
+            <div className="container">
+                <div className="days-topics">
+                    <h1>Todays topics section</h1> 
+                </div>
+                <GoogleCalendar width={'500px'} height={'500px'} calendarID={''} /*calendarID can be dynamic to each cohort, for now defaults to quebec*/ />
+            
+            </div>
+           
+            <div className="lecture-readme">
+                <h1>This will be the readme for the days lecture</h1> 
+            </div>
+        </div>
+    )
+}
+
+export default StudentPage
