622b2182f78a12fe84e9dc39b39fa411a34f2eb1
Aidan Matchette <frigasack@gmail.com>
2022-05-03 16:47:32 -0700

    created an instructor page

--- src/pages/InstructorPage.js
+++ src/pages/InstructorPage.js
@@ -1,31 +1,21 @@
 import GoogleCalendar from "../components/GoogleCalendar";
-import { DayContext } from "../context/DayProvider";
 import { useContext, useEffect } from "react";
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 import theme from '../utils/theme.js'
 import { ThemeProvider } from "@mui/material/styles";
 import { Button, Box } from '@mui/material'
-import { useAxios } from "../utils/useAxios";
-
+import { DayContext } from "../context/DayProvider";
 
 function InstructorPage() {    
-    const { landingRaw, setDirty } = useContext(DayContext) 
-    console.log(landingRaw)
-    const backend = useAxios()
     
-    const handleGenerateCheckin = () => {
-        console.log('generate check-in')
-        backend.post('/api/instructor/checkin/').then(response => {
-           console.log(response)
-            setDirty(true)
-        })
-    }
-           
+    const { landingRaw } = useContext(DayContext) 
+    console.log(landingRaw)
+    
+          
     return (
         <ThemeProvider theme={theme}>
             <Box sx={{display: 'flex', justifyContent: 'center'}}>
-                <Button color="secondary" variant="contained" onClick={handleGenerateCheckin} sx={{width: '80%', mt: 3}}>Generate Daily Check-In</Button>
             </Box>
             <div className="student-landing-container">
                 <div className="google-calander">
