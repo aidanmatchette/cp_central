def31cf9129c097eddf8f3b31c906bf8b20f3758
Aidan Matchette <frigasack@gmail.com>
2022-05-02 19:25:39 -0700

    git test

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,8 +4,8 @@ export default function HomePage() {
 
     return (
         <div>
-            <h1>This is the Un-Authenticated landing page</h1>
+            <h1>Un-Authenticated landing page</h1>
             <GoogleCalendar width={'500px'} height={'500px'} calendarID={''} /*calendarID can be dynamic to each cohort, for now defaults to quebec*/ />
         </div>
     )
-}
+} 
