830ff8a3f886309a1ba6fa03481403dba92e3ac7
Aidan Matchette <frigasack@gmail.com>
2022-05-03 08:39:08 -0700

    basic layout for organization of data in studentpage

--- src/pages/StudentPage.js
+++ src/pages/StudentPage.js
@@ -3,18 +3,24 @@ import GoogleCalendar from "../components/GoogleCalendar";
 function StudentPage() {    
     return (
         <div>
-            <h1>This is the Authenticated student landing page</h1>
             <div className="student-landing-container">
-                <div className="days-topics">
-                    <h1>Todays topics section</h1> 
+               <div className="google-calander">
+                    <GoogleCalendar width={'500px'} height={'500px'} calendarID={''} /*calendarID can be dynamic to each cohort, for now defaults to quebec*/ />
                 </div>
-                <GoogleCalendar width={'500px'} height={'500px'} calendarID={''} /*calendarID can be dynamic to each cohort, for now defaults to quebec*/ />
+                <div className="todays-info">
+                    <div className="days-topics">
+                        <h1>Todays topics section</h1> 
+                    </div>
+                    <div className="lecture-readme">
+                        <h1>This will be the readme for the days lecture</h1> 
+        
+                    </div>
+                </div>
+               
+                 
+            </div>
             
-            </div>
            
-            <div className="lecture-readme">
-                <h1>This will be the readme for the days lecture</h1> 
-            </div>
         </div>
     )
 }
