bfce00bb235f0d5f41dd43225bb02751f5724b2f
John Winko <john.winko@gmail.com>
2022-05-03 19:17:33 -0500

    added cohorts/groups, changed dates of lessons to this week to fixture data. added admin registers for cohort and checkins. Fixed wires on signup dropdown.

--- app/models/lesson.py
+++ app/models/lesson.py
@@ -23,6 +23,7 @@ class Lesson(models.Model):
     date = models.DateField(null=True, blank=True)
     start_time = models.DateTimeField(null=True, blank=True)
     stop_time = models.DateTimeField(null=True, blank=True)
+    markdown = models.TextField(default=" ")
 
     def __str__(self):
         return f'{self.topic.title} - {self.title}'
