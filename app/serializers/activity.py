4b1b233a0b9adefb39043c781f2bafce0fd8ebe1
markossankey <markossankey@icloud.com>
2022-05-03 21:11:36 -0500

    added simpleUserSerializer to make activity_groups easier to use

--- app/serializers/activity.py
+++ app/serializers/activity.py
@@ -1,11 +1,17 @@
 from rest_framework import serializers
-from app.models import Activity, ActivityGroup
+from app.models import Activity, ActivityGroup, User
+
+class SimpleUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name']
 
 
 class ActivityGroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = ActivityGroup
         fields = "__all__"
+    members = SimpleUserSerializer(many=True, read_only=True)
 
 
 class ActivitySerializer(serializers.ModelSerializer):
