42f97084c99269cad303fa3c7c7e13e45b4ede5b
markossankey <markossankey@icloud.com>
2022-05-04 18:26:29 -0500

    created route and hanlder for api/roster/randomize/

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,4 +1,4 @@
-from .user import UserViewSet, signup, AppointmentViewSet, UserLinkViewSet, roster, super_info
+from .user import UserViewSet, signup, AppointmentViewSet, UserLinkViewSet, roster, super_info, create_random_groups
 from .choices import all_choices, get_all_choices
 from .questionnaire import QuestionnaireViewSet, FilledQuestionnaireViewSet
 from .activity import ActivityViewSet, ActivityGroupViewSet
