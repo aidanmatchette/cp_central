bd6e755b17054d79e3a96b9fd478fb339d7fbcf0
John Winko <john.winko@gmail.com>
2022-05-05 11:37:15 -0500

    added search functionality

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import { DayProvider } from "./context/DayProvider";
 import StudentApprovalPage from './pages/StudentApproval/StudentApprovalPage';
 import BioPage from "./pages/BioPage";
 import ClassRosterPage from "./pages/ClassRosterPage"
+import SearchResults from "./pages/SearchResults";
 
 function App() {
 
@@ -30,6 +31,7 @@ function App() {
                         <Route path={"/pending-students"} element={<StudentApprovalPage />} />
                         <Route path={"/biopage/:userID"} element={<BioPage />}/>
                         <Route path={"/class-roster"} element={<ClassRosterPage />}/>
+                        <Route path={"/search/:keyword"} element={<SearchResults />}/>
                     </Route>
                 </Routes>
             </DayProvider></AuthProvider>
