5179043b1b358d8204cf23ece928d1382b15f8e0
John Winko <john.winko@gmail.com>
2022-05-08 14:00:30 -0500

    created different random group generator (not wired in yet)

--- app/helpers/__init__.py
+++ app/helpers/__init__.py
@@ -1,1 +1,2 @@
 from .github_api import get_daily_readme_from_gh
+from .random_generator import random_generator
--- app/views/activity.py
+++ app/views/activity.py
@@ -1,7 +1,8 @@
 from django.http import JsonResponse
-from rest_framework.decorators import api_view, action, permission_classes
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
+from app.helpers import random_generator
 from app.models import Activity, ActivityGroup
 from app.serializers import ActivitySerializer, ActivityGroupSerializer
 
@@ -16,6 +17,28 @@ class ActivityViewSet(ModelViewSet):
         if date is not None:
             return Activity.objects.filter(date=date)
         return super().get_queryset()
+
+    @action(methods=['POST'], detail=False)
+    def random_group(self, request, pk=None):
+        date = request.data['date'] if 'date' in request.data else None
+        group_size = int(request.data['group_size']) if 'group_size' in request.data else 2
+        final_groups = random_generator(request, date, group_size)
+        activity = Activity(
+            group=request.user.default_group,
+            name=request.data['name'],
+            date=date,
+            size=group_size
+        )
+        activity.save()
+
+        for group in final_groups:
+            activity_group = ActivityGroup(activity=activity)
+            activity_group.save()
+            for person in group:
+                activity_group.members.add(person)
+
+        return JsonResponse(ActivitySerializer(activity).data)
+
 
 class ActivityGroupViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
