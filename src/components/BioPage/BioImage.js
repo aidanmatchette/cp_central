04f313dea13df6c852cfe288fc03a4db579071a7
Aidan Matchette <frigasack@gmail.com>
2022-05-10 08:40:05 -0700

    added blank profile pic to bio page

--- src/components/BioPage/BioImage.js
+++ src/components/BioPage/BioImage.js
@@ -1,31 +1,57 @@
-import {useAxios} from "../../utils/useAxios";
+import { useAxios } from "../../utils/useAxios";
+import { Avatar } from "@mui/material";
+import { useState, useEffect } from "react";
 
-export default function BioImage({canEdit, avatar, refreshData, userID}) {
-    const backend = useAxios()
+export default function BioImage({ canEdit, avatar, refreshData, userID }) {
+  const backend = useAxios();
+  const [validPic, setValidPic] = useState(true);
+  const imageChange = async (e) => {
+    let formData = new FormData();
+    formData.append("avatar", e.target.files[0]);
+    const headers = {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    };
+    await backend.patch(`/api/v1/user/${userID}/`, formData, headers);
+    refreshData();
+  };
+  const pictureError = () => {
+    return <Avatar src="/broken-image" />;
+  };
+  return (
+    <>
+      {validPic ? (
+        <img
+          src={avatar}
+          alt={"Avatar"}
+          width={"100%"}
+          onError={() => setValidPic(false)}
+          style={{ borderRadius: "30%", width: "300px" }}
+        />
+      ) : (
+        <Avatar
+          src="/broken-image"
+          sx={{
+            height: 200,
+            width: 200,
+            margin: "auto",
+            mb: 3,
+            display: "flex",
+            justifyContent: "center",
+          }}
+        />
+      )}
 
-    const imageChange = async (e) => {
-        let formData = new FormData()
-        formData.append("avatar", e.target.files[0])
-        const headers = {
-            headers: {
-                "Content-Type": "multipart/form-data",
-            }
-        }
-        await backend.patch(`/api/v1/user/${userID}/`, formData, headers)
-        refreshData()
-    }
-
-    return (
-        <>
-            <img src={avatar} alt={"Avatar"} width={'100%'} style={{borderRadius: "30%", width: "300px"}}/>
-
-            {canEdit &&
-                <input accept={"image/*"}
-                       type={"file"}
-                       name={"avatar"}
-                       onChange={imageChange}
-                />
-            }
-            <br/></>
-    )
+      {canEdit && (
+        <input
+          accept={"image/*"}
+          type={"file"}
+          name={"avatar"}
+          onChange={imageChange}
+        />
+      )}
+      <br />
+    </>
+  );
 }
