38f521fc96e6af337ac139e22fbd006d8b76becc
John Winko <john.winko@gmail.com>
2022-05-10 12:52:02 -0500

    fixed issue with class roster key

--- src/pages/ClassRosterPage.js
+++ src/pages/ClassRosterPage.js
@@ -70,7 +70,7 @@ export default function ClassRosterPage() {
         }
         return (
             <TableRow
-                key={member.first_name}
+                key={member.id}
                 sx={{"&:last-child td, &:last-child th": {border: 0}, backgroundColor: presentColor(member)}}
             >
                 <TableCell component="th" scope="row">
